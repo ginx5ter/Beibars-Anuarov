67ecbc15fc558b908474dd0644e7c61a70dabab7
ginx5ter <96648705+ginx5ter@users.noreply.github.com>
2022-02-21 06:46:35 +0600

    Add files via upload

---
+++ last assingment1/contact.js
@@ -0,0 +1,6 @@
+function myFunction() {
+
+    var audio = new Audio('done.mp3');
+    audio.play();
+    alert("Success");
+}
---
+++ last assingment1/validation.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $("#registration").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+
+            },
+            surname: {
+                required: true,
+                minlength: 3
+            },
+            username: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            pass: {
+                required: true,
+                minlength: 8
+            },
+            cpass: {
+                required: true,
+            }
+        },
+        messages: {
+            name: {
+                required: 'Name is required!'
+            },
+            surname: {
+                required: 'Surname is required!'
+            },
+            username: {
+                required: 'Username is required!'
+            },
+            email: {
+                required: 'Email is required!'
+            },
+            pass: {
+                required: 'Password is required!'
+            },
+            cpass: {
+                required: 'Confirmation is required!'
+            }
+        }
+
+    });
+});
